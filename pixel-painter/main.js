7f2258d59e279fa8b0c75b61993266c6ee20ee64
AntCannon <anthonycannonier@gmail.com>
2024-03-08 20:12:10 -0500

    add user input grid size, submit and clear button features

--- pixel-painter/main.js
+++ pixel-painter/main.js
@@ -1,15 +1,39 @@
+// number of squares across
+const form = document.createElement("form");
+const numSquaresInput = document.createElement("input");
+numSquaresInput.setAttribute("type", "number" );
+numSquaresInput.setAttribute("min", "1" );
+numSquaresInput.setAttribute("max", "20" );
+numSquaresInput.setAttribute("placeholder", "Number of Squares");
+numSquaresInput.style.width = "100px"
+numSquaresInput.value = "10"
+
+const submitButton = document.createElement("button");
+submitButton.innerText = "Submit";
+
+
+form.appendChild(numSquaresInput);
+form.appendChild(submitButton);
+
+form.style.display = "block"
+form.style.marginBottom = "20px"
+form.style.textAlign = "center"
+
+const header = document.querySelector("header");
+header.after(form)
+
 // change number of columns in canvas
 const canvas = document.querySelector("#canvas");
 
 // get the width of the canvas
-const canvasWidth = +window.getComputedStyle(canvas).width.slice(0, -2);
+let canvasWidth = +window.getComputedStyle(canvas).width.slice(0, -2);
 
 // set the number of columns
-const numColumns = 10;
+let numColumns = numSquaresInput.value;
 
 // cell calculations
 // cell width = canvas width / numColumns
-const cellWidthPx = (canvasWidth / numColumns).toString() + "px";
+let cellWidthPx = (canvasWidth / numColumns).toString() + "px";
 
 // change the grid-template-columns in the canvas
 canvas.style.gridTemplateColumns = `repeat(${numColumns}, ${cellWidthPx})`;
@@ -28,16 +52,52 @@ for (let color of colorCollection) {
   })
 }
 
-// loop the create creation of divs to fill canvas
-for (let i = 1; i <= numColumns**2; i++) {
-  // create div
-  const cell = document.createElement("div");
-  // add .cell class
-  cell.classList.add("cell");
-  //add event listener
-  cell.addEventListener("click", (event) => {
-    cell.style.background = currentColor.style.background;
-  })
-  // append to canvas
-  canvas.appendChild(cell);
+// get all cells
+let allCells = document.querySelectorAll(".cell")
+
+function generateCanvas() {
+  if (numSquaresInput.value < 2) {
+    numSquaresInput.value = 2
+  } 
+  if (numSquaresInput.value > 20) {
+    numSquaresInput.value = 20
+  } 
+  numColumns = numSquaresInput.value;
+  cellWidthPx = (canvasWidth / numColumns).toString() + "px";
+  canvas.style.gridTemplateColumns = `repeat(${numColumns}, ${cellWidthPx})`;
+  canvas.innerHTML = "";
+
+  // loop the create creation of divs to fill canvas
+  for (let i = 1; i <= numColumns**2; i++) {
+    // create div
+    const cell = document.createElement("div");
+    // make the cells square
+    cell.style.width = cellWidthPx;
+    cell.style.height = cellWidthPx;
+  
+    // add .cell class
+    cell.classList.add("cell");
+    //add event listener
+    cell.addEventListener("click", (event) => {
+      cell.style.background = currentColor.style.background;
+    })
+    // append to canvas
+    canvas.appendChild(cell);
+  }
+
+  // preload all cells
+  allCells = document.querySelectorAll(".cell")
+}
+
+generateCanvas();
+
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault()
+  generateCanvas(); 
+})
+
+function canvasFill() {
+  for (let cell of allCells) {
+    cell.style.background = currentColor.style.background
+  }
 }

6b9d8053c283d51a513ec8a13786e6531102cb71
AntCannon <anthonycannonier@gmail.com>
2024-03-09 19:50:35 -0500

    start make move

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -6,11 +6,31 @@ function makeBoard() {
     square.classList.add("square");
     square.classList.add("empty");
     board.appendChild(square);
+    
   }
 }
 
-makeBoard();
 
 // - [ ] Call `makeBoard()` on page load so that 9 squares appear and the board is 3 x 3 squares.
+makeBoard();
 
 // - [ ] Write a function called `makeMove()` that is an event handler for the `div`s with the class of `square` that
+// - Checks if the clicked square has the class `empty`.
+// - If the square has the class empty add either an `X` or an `O` inside the square, remove the class `empty` and write some logic so that the next time this function is called the other player has a play (first play an X then an O, then an X...).
+// - Players should NOT be able to change a full square.
+
+let playerTurn = 0;
+
+function makeMove() {
+  const squares = document.querySelectorAll(".square");
+  for (let square of squares) {
+    square.addEventListener("click", () => {
+      if (square.classList.contains("empty")) {
+        console.log("square is empty");
+        square.innerText = "X";
+      }
+    })
+  }
+}
+
+makeMove();

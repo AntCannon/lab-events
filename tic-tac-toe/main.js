e6d9c1aeb96f48d54ec20cd3f4105e180d4926c7
AntCannon <anthonycannonier@gmail.com>
2024-03-09 23:19:18 -0500

    add game logic and winner alert

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -1,13 +1,14 @@
 // get board
 const board = document.querySelector(".tic-tac-toe");
+const boardMap = {};
 
-// - [ ] Write a function called `makeBoard()` that will generate 9 `div` with the class `empty` and another class `square`.
 function makeBoard() {
   for (let i = 0; i < 9; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
     square.classList.add("empty");
-    square.classList.add(`${String.fromCharCode(97 + i)}`)
+    const spot = String.fromCharCode(97 + i);
+    square.classList.add(`${spot}`)
     makeMove(square);
     board.appendChild(square);
   }
@@ -18,30 +19,50 @@ makeBoard();
 // get squares
 const squares = document.querySelectorAll(".square");
 
+// get and update spot nodes
+let a, b, c, d, e, f, g, h, i;
+function updateSpots() {
+  a = document.querySelector(".a").innerText
+  b = document.querySelector(".b").innerText
+  c = document.querySelector(".c").innerText
+  d = document.querySelector(".d").innerText
+  e = document.querySelector(".e").innerText
+  f = document.querySelector(".f").innerText
+  g = document.querySelector(".g").innerText
+  h = document.querySelector(".h").innerText
+  i = document.querySelector(".i").innerText
+}
 // check winner
-const boardMap = new Map();
+function hasWinner(square) {
+  if (a && a === b && b === c ||
+    d && d === e && e === f ||
+    g && g === h && h === i ||
 
-console.log([...squares.entries()][0][1].classList[2]);
+    a && a === d && d === g ||
+    b && b === e && e === h ||
+    c && c === f && f === i ||
 
-
-function hasWinner() {
-  
+    a && a === e && e === i ||
+    c && c === e && e === g) {
+      setTimeout(() => {
+        alert(`${square.innerText} is the Winner!`)
+      }, 250);
+    }
 }
 
-
+// player turns
 let turns = 0;
-
-// display players Turn
 const playerNode = document.querySelector(".player");
 playerNode.innerText = "X"
 
 function makeMove(square) {
-  square.addEventListener("click", () => {
+  square.addEventListener("click", (e) => {
     if (square.classList.contains("empty")) {
       square.classList.remove("empty");
       square.innerText = playerNode.innerText;
+      updateSpots();
+      if (turns > 3) hasWinner(square);
       playerNode.innerText = playerNode.innerText === "X" ? "O" : "X";
-
       turns++;
       isGameOver();
     }
@@ -56,12 +77,11 @@ button.addEventListener("click", () => {
   turns = 0;
 })
 
-// game over alert
+// turn game over alert
 function isGameOver() {
   if (turns === 9) {
     setTimeout(() => {
-      alert("Game is Over")}, 250);
+      alert("Game is Over")
+    }, 250);
   }
 }
-
-
